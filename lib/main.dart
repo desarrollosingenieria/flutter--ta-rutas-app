594eab4da606a905985950687b75431dd32ab3f8
Facundo Barreto <facubrt@gmail.com>
2024-05-16 08:47:49 -0300

    splash generado

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import 'package:tarutas/src/data/local/user_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   final localDB = LocalData();
   await localDB.init();
   await localDB.openBox();
--- lib/src/theme/color_app.dart
+++ lib/src/theme/color_app.dart
@@ -8,9 +8,5 @@ class ColorApp {
   static const Color secondaryDark = Color(0xFF1B1E41);
   static const Color tertiaryDark = Color(0xFF2C2F57);
 
-  static const Color azure = Color(0xFF0094FF);
   static const Color brandblue = Color(0xFF003A70);
-  static const Color padua = Color(0xFF81B996);
-  static const Color coral = Color(0xFFFF7B51);
-  static const Color rajah = Color(0xFFFEB95E);
 }
